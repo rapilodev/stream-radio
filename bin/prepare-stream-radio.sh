990b5f306271578d0d4ae7c14296ba30d78f6d21
Milan <mc@radiopiloten.de>
2020-11-21 22:54:03 +0100

    rename stream-schedule/(radio) to stream-radio/

--- bin/prepare-stream-radio.sh
+++ bin/prepare-stream-radio.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-pkill -9 -f "/etc/stream-schedule/radio/start.conf"
+pkill -9 -f "/etc/stream-radio/start.conf"
 
-[ -d /var/run/stream-schedule ] || mkdir /var/run/stream-schedule
-chmod 775 /var/run/stream-schedule
-chown audiostream /var/run/stream-schedule
+[ -d /var/run/stream-radio ] || mkdir /var/run/stream-radio
+chmod 775 /var/run/stream-radio
+chown audiostream /var/run/stream-radio
 
-[ -f /var/run/stream-schedule/stream-radio.pid ] && rm /var/run/stream-schedule/stream-radio.pid
+[ -f /var/run/stream-radio/stream-radio.pid ] && rm /var/run/stream-radio/stream-radio.pid
 
-[ -d /var/log/stream-schedule/ ] || mkdir /var/log/stream-schedule/
-chmod 755 /var/log/stream-schedule
-chown audiostream:www-data /var/log/stream-schedule/
-[ -f /var/log/stream-schedule/radio.log ] && chmod 664 /var/log/stream-schedule/radio.log
-[ -f /var/log/stream-schedule/radio.log ] && chown audiostream:www-data /var/log/stream-schedule/radio.log
+[ -d /var/log/stream-radio/ ] || mkdir /var/log/stream-radio/
+chmod 755 /var/log/stream-radio
+chown audiostream:www-data /var/log/stream-radio/
+[ -f /var/log/stream-radio/radio.log ] && chmod 664 /var/log/stream-radio/radio.log
+[ -f /var/log/stream-radio/radio.log ] && chown audiostream:www-data /var/log/stream-radio/radio.log
 
 exit 0
